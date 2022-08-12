a8aaefe640f5768990ca0bd226040e72a3a60f43
Romashka <rroma13579@gmail.com>
2022-08-12 23:48:58 +0200

    third commit

--- js/script.js
+++ js/script.js
@@ -1,33 +1,30 @@
-function Btn_Clock(){
-    var btn_status = new Boolean(true)
-    MyTime(btn_status)
-}
-function Btn_Timer(){
-    var btn_status = new Boolean(false)
-    MyTime(btn_status)
-}
-function MyTime(btn_status) {
+
+function MyTime() {
     var time = new Date()
     document.getElementById("clock").innerHTML = time.toLocaleTimeString()
-    var time = new Date(0,0,0)
+    var time = new Date(0, 0, 0)
     document.getElementById("timer").innerHTML = time.toLocaleTimeString()
-    if(btn_status == false){
+    if (document.getElementById('btn-clock').checked){
         document.getElementById("clock").style.fontSize = "80px"
         document.getElementById("clock").style.color = "transparent"
-        setInterval(Changes,1000)
+        document.getElementById("timer").style.display = "block"
+        document.getElementById("timer").style.fontSize = "40px"
+        document.getElementById("timer").style.color = "white"
     }
-    else if (btn_status == true){
+    else if (document.getElementById('btn-timer').checked){
         document.getElementById("timer").style.fontSize = "0px"
         document.getElementById("timer").style.color = "transparent"
-        setInterval(Changes_false,1000)
+        document.getElementById("clock").style.display = "block"
+        document.getElementById("timer").style.fontSize = "40px"
+        document.getElementById("timer").style.color = "white"
     }
 }
-function Changes(){
+function Changes() {
     document.getElementById("timer").style.fontSize = "40px"
     document.getElementById("timer").style.color = "white"
     document.getElementById("clock").style.display = "none"
 }
-function Changes_false(){
+function Changes_false() {
     document.getElementById("clock").style.fontSize = "40px"
     document.getElementById("clock").style.color = "white"
     document.getElementById("timer").style.display = "none"
